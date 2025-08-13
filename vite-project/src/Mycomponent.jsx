8dc74b0d5212620ee9c1ba01cd3bada56ad1b737
Satti Krishna Tulasi <krishnatulasi0302@gmail.com>
2025-08-13 11:33:29 +0530

    Commit

---
+++ vite-project/src/Mycomponent.jsx
@@ -0,0 +1,11 @@
+import React,{Component} from "react";
+
+class Mycomponent extends Component {
+    render() {
+        return (
+            <h1>This is a Class Component</h1>
+        )
+    }
+}
+
+export default Mycomponent;
