fb2b2d34402babb1a7c92a65e4b3c5ab191b38e6
Satti Krishna Tulasi <krishnatulasi0302@gmail.com>
2025-09-03 10:45:36 +0530

    Commit

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,7 +1,8 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Mycomponent from './Mycomponent'
+import ClassComponent from './ClassComponent'
+import FunctionComponent from './FunctionComponent'
 import Components from './Components'
 
 function App() {
@@ -9,10 +10,12 @@ function App() {
   return (
     <>
       <h1>This is App.jsx Component</h1>
-      {/* <Mycomponent /> */}
+      <ClassComponent/>
+      <FunctionComponent/>
       <Components/>
     </>
   )
 }
 
 export default App
+
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,14 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Mycomponent from './Mycomponent.jsx'
+
 import FunctionComponent from './FunctionComponent.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
       <App />
-      <Mycomponent />
-      <FunctionComponent />
   </StrictMode>,
 )
